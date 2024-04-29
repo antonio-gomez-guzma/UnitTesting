71165587d7be048c8acff2e8af540e2d8a2a78ef
AVANADE-CORP\antonio.gomez.guzman <antonio.gomez.guzman@avanade.com>
2024-04-29 12:56:59 +0200

    Modulo 5 completo

--- UnitTesting/EmployeeManagement.Test/DataDrivenEmployeeServiceTests.cs
+++ UnitTesting/EmployeeManagement.Test/DataDrivenEmployeeServiceTests.cs
@@ -1,5 +1,6 @@
 ﻿using EmployeeManagement.DataAccess.Entities;
 using EmployeeManagement.Test.Fixtures;
+using EmployeeManagement.Test.TestData;
 using Xunit;
 
 namespace EmployeeManagement.Test
@@ -62,5 +63,68 @@ namespace EmployeeManagement.Test
             // Assert
             Assert.False(internalEmployee.MinimumRaiseGiven);
         }
+
+        public static IEnumerable<object[]> ExampleTestDataForGiveRaise_WithProperty
+        {
+            get
+            {
+                return new List<object[]>
+                {
+                        new object[] { 100, true },
+                        new object[] { 200, false }
+                };
+            }
+        }
+
+        public static TheoryData<int,bool> StronglyTypedExampleTestDataForGiveRaise_WithProperty
+        {
+            get
+            {
+                return new TheoryData<int, bool>()
+                {
+                        { 100, true },
+                        { 200, false }
+                };
+            }
+        }
+
+        public static IEnumerable<object[]> ExampleTestDataForGiveRaise_WithMethod(
+             int testDataInstancesToProvide)
+        {
+            var testData = new List<object[]>
+                {
+                        new object[] { 100, true },
+                        new object[] { 200, false }
+                };
+
+            return testData.Take(testDataInstancesToProvide);
+        }
+
+
+        [Theory]
+        //[MemberData(
+        //    nameof(DataDrivenEmployeeServiceTests.ExampleTestDataForGiveRaise_WithMethod),
+        //    1,
+        //    MemberType = typeof(DataDrivenEmployeeServiceTests))]
+        //[ClassData(typeof(EmployeeServiceTestData))]
+        //[ClassData(typeof(StronglyTypedEmployeeServiceTestData))]
+        //[MemberData(nameof(StronglyTypedExampleTestDataForGiveRaise_WithProperty))]
+        [ClassData(typeof(StronglyTypedEmployeeServiceTestData_FromFile))]
+        public async Task GiveRaise_RaiseGiven_EmployeeMinimumRaiseGivenMatchesValue(
+            int raiseGiven, bool expectedValueForMinimumRaiseGiven)
+        {
+            // Arrange  
+            var internalEmployee = new InternalEmployee(
+                "Brooklyn", "Cannon", 5, 3000, false, 1);
+
+            // Act
+            await _employeeServiceFixture.EmployeeService.GiveRaiseAsync(
+                internalEmployee, raiseGiven);
+
+            // Assert
+            Assert.Equal(expectedValueForMinimumRaiseGiven, 
+                internalEmployee.MinimumRaiseGiven);
+        }
+
     }
 }
